5f7f9bd5c5e32b972ed49594b149d297d5670907
cwrun <297cwrun@gmail.com>
2021-01-23 12:32:08 +0800

    feat: 支持转发分享

--- js/main.js
+++ js/main.js
@@ -2,7 +2,26 @@ import Stack from './object/Stack'
 import EventEmiter  from './utils/eventEmiter'
 window.$on = EventEmiter.on.bind(EventEmiter)
 window.$emit = EventEmiter.emit.bind(EventEmiter)
-import Audio from './object/Audio'
+// 允许分享
+wx.showShareMenu({
+  withShareTicket: true,
+  menus: ['shareAppMessage', 'shareTimeline']
+})
+wx.onShareAppMessage(() => {
+  return {
+    title: 'Perfect Stack',
+    imageUrl: '' // 图片 URL
+  }
+})
+
+// 允许分享到朋友圈
+wx.onShareTimeline(() => {
+  return {
+    title: 'Perfect Stack',
+    imageUrl: '', // 图片 URL
+  }
+})
+
 /**
  * 游戏主函数
  */
